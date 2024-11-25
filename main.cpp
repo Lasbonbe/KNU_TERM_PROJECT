4ac20d73ec060fa72608fc8f2cc4c28aa75932d2
lasbo <mathis.henry@edu.ece.fr>
2024-11-25 21:35:30 +0900

    opencv install

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-int main() {
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/core/utility.hpp>
+
+int main()
+{
+
+    cv::namedWindow("test", cv::WINDOW_NORMAL);
+    // Hello, World!
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
