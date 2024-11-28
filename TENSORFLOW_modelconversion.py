6229ceb345e78090cac788ab214171c022791ada
lasbo <mathis.henry@edu.ece.fr>
2024-11-29 00:25:56 +0900

    added 1.15 context

--- TENSORFLOW_modelconversion.py
+++ TENSORFLOW_modelconversion.py
@@ -8,9 +8,11 @@ import tensorflow as tf
 #░░░██╔══██║╚════██╗        ░░░██║░░░██║░░██║       ░░░██║░░██║██║╚████║██║╚████║░██╔██╗░
 #██╗██║░░██║██████╔╝        ░░░██║░░░╚█████╔╝       ██╗╚█████╔╝██║░╚███║██║░╚███║██╔╝╚██╗
 #╚═╝╚═╝░░╚═╝╚═════╝░        ░░░╚═╝░░░░╚════╝░       ╚═╝░╚════╝░╚═╝░░╚══╝╚═╝░░╚══╝╚═╝░░╚═╝
-# This code snippet is used to convert a TensorFlow model to an ONNX model
+# This code  is used to convert a TensorFlow model to an ONNX model
 # Because OpenCV does not support  .h5 files, we need to convert the model to an ONNX format
 # Replace the MODELNAME with the name of your model
+#⚠️ SPECIAL TENSORFLOW VERSION REQUIRED: 1.15.0 ---> PYTHON VERSION MUST BE DOWNGRADED <= 3.7, USE PYTORCH FOR EASY CONVERSION ⚠️
+
 #********************************************************************************************************************
 
 MODELNAME = "datasetV2"
